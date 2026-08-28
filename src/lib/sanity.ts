8d09d91671fa7bb7610918dbf1185b47682daa28
Sergejs <sergejs@isimple.lv>
2026-08-28 16:31:10 +0300

    fix: sort iphone models by createdAt asc

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -338,7 +338,7 @@ export async function getIPhoneModels() {
     return MOCK_IPHONE_MODELS;
   }
   try {
-    const query = `*[_type == "iphone_model"] | order(name desc) {
+    const query = `*[_type == "iphone_model"] | order(_createdAt asc) {
       name,
       "slug": slug.current,
       prices,
